71a6db3f1d38b9b620ab4db91eb1c42d731e622d
anhnx4 <anh833905@gmail.com>
2024-10-16 10:25:28 +0700

    move connections

--- src/server.js
+++ src/server.js
@@ -1,10 +1,16 @@
 require("dotenv").config();
-
 const express = require("express");
+const configViewEngine = require("./config/viewEngine");
+
 const app = express();
 const port = process.env.PORT || 8081;
-const configViewEngine = require("./config/viewEngine");
 const webRouter = require("./routes/web");
+const connection = require("./config/database");
+
+connection.query("select * from User", function (error, results, fields) {
+  if (error) throw error;
+  console.log("The results is: ", results);
+});
 
 //config static files
 configViewEngine(app);
